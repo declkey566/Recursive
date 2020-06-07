2121ae679e3bea54f879f26f40d8bad1eb266857
Declan k <decktehboss@hotmail.com>
2020-06-07 14:33:17 -0400

    Add project files.

---
+++ Recursive/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Recursive
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(33, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fibonnaci I";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label1.Location = new System.Drawing.Point(30, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(301, 186);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(394, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(150, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 28);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Enter Value to Solve:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(470, 241);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(301, 186);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("MV Boli", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(565, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 45);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Sum ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button3.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(227, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 45);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Fibonnaci R";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Recursive/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace Recursive
+{
+    public partial class Form1 : Form
+    {
+        Stopwatch watch1 = new Stopwatch();
+        Stopwatch watch2 = new Stopwatch();
+        Stopwatch watch3 = new Stopwatch();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        static int Fibonacci(int n)
+        {
+            int firstnumber = 0, secondnumber = 1, result = 0;
+            if (n == 0) return 0; //It will return the first number of the series
+            if (n == 1) return 1; // it will return  the second number of the series
+            for (int i = 2; i <= n; i++)  // main processing starts from here
+            {
+                result = firstnumber + secondnumber;
+                firstnumber = secondnumber;
+                secondnumber = result;
+            }
+            return result;
+        }
+        
+
+        static int FibonacciR(int n)
+        {
+            int firstnumber = 0, secondnumber = 1, result = 0;
+            if (n == 0) return 0; //it will return the first number of the series
+            if (n == 1) return 1; // it will return the second number of the series
+            return FibonacciR(n - 1) + FibonacciR(n - 2);
+        }
+
+        static int sumOfDigitsFrom1ToN(int n)
+        {
+
+            // initialize result 
+            int result = 0;
+
+            // One by one compute sum of digits 
+            // in every number from 1 to n 
+            for (int x = 1; x <= n; x++)
+                result += sumOfDigits(x);
+
+            return result;
+        }
+        static int sumOfDigits(int x)
+        {
+            int sum = 0;
+
+            while (x != 0)
+            {
+                sum += x % 10;
+                x = x / 10;
+            }
+
+            return sum;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            watch1.Start();
+            int length = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < length; i++)
+            {
+                label1.Text = ("The Interative search of " + (length) + " is: " + Fibonacci(i) + "\n" + "Completed in: " + watch1.ElapsedMilliseconds + " ms");
+            }
+            watch1.Stop();
+            watch1.Reset();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            watch2.Start();
+            int length = Convert.ToInt32(textBox1.Text);
+            for (int i = 0; i < length; i++)
+            {
+                label1.Text = ("The Recursive search of " + (length) + " is: " + FibonacciR(i) + "\n" + "Completed in: " + watch2.ElapsedMilliseconds + " ms");
+            }
+            watch2.Stop();
+            watch2.Reset();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            watch3.Start();
+            int n = Convert.ToInt32(textBox1.Text);
+            label3.Text = ("The sum of digits from 1 to " + n + " is " + sumOfDigitsFrom1ToN(n) + " Completed in: " + watch2.ElapsedMilliseconds + " ms");
+            watch3.Stop();
+            watch3.Reset();
+        }
+    }
+    
+}
